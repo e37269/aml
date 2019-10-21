f33ca2bcaeec193369668edebd065720ba680e2d
anderer.weber <anderer.weber@gmail.com>
2019-10-21 15:38:24 +0200

    - Nutzerdaten entfernt

--- anrufliste/connect_ls.php
+++ anrufliste/connect_ls.php
@@ -1,5 +1,5 @@
 <?php
-$ls = mysqli_connect('192.168.155.155', 'aml', 'YA2lS8OA', 'leitstelle');
+$ls = mysqli_connect('host', 'user', 'passwort', 'datenbank');
 if(!$ls)
 {
 	exit("Verbindungsfehler Datenbank leitstelle: ".mysqli_connect_error());
