1e41979252114af340e0682fcc00696502a4ecba
anderer.weber <anderer.weber@gmail.com>
2019-10-24 12:29:03 +0200

    ...

--- config.py
+++ config.py
@@ -3,24 +3,24 @@
 """
 IP-Adresse oder DNS-Namen eures C4 MariaDB-Servers, sowie Nutzername und Passwort
 """
-db_c4 = {'host': '192.168.155.163',
-         'user': 'aml',
-         'password': 'YA2lS8OA',
+db_c4 = {'host': '***',
+         'user': '***',
+         'password': '***',
          'db': 'c4'}
 
 """
 IP-Adresse oder DNS-Namen eures Servers, auf dem die Hilfsdatenbank 'leitstelle' läuft, sowie Nutzername und Passwort
 """
-db_leitstelle = {'host': '192.168.155.155',
-         'user': 'aml',
-         'password': 'YA2lS8OA',
+db_leitstelle = {'host': '***',
+         'user': '***',
+         'password': '***',
          'db': 'leitstelle'}
 
 """
 Zugangsdaten für den AML-Server
 """
-aml_server = {'user': 'lst_werra_meissner_kreis',
-              'password': 'd5ae58df8dd1a0a46a57415cd70112ec'}
+aml_server = {'user': '***',
+              'password': '***'}
               
 """
 Query C4 Anrufe (verarbeiet werden ROW_ID, telefonnummer, anrufzeit)
